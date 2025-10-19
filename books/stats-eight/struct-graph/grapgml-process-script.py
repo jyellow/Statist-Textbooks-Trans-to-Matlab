760d4657aaf7b4e78ccd6deb36d5a900cf5e7e25
jyellow <27905024+jyellow@users.noreply.github.com>
2025-10-19 17:30:50 +0800

    修改解析graphml文件内容的脚本代码，以及graphml的自定义字段的名称
    自定义字段“Matlab文档链接”改为“ matlab_doc_links”
    resolve #16

--- books/stats-eight/struct-graph/grapgml-process-script.py
+++ books/stats-eight/struct-graph/grapgml-process-script.py
@@ -2,15 +2,32 @@ import networkx as nx
 import pandas as pd
 
 # 1. 读取 GraphML 文件
-graphml_file_path = '/path/to/graphml_file/chapter-03-index.graphml'
+graphml_file_path = '/Users/johannyellow/Documents/MATLAB/Statist-Textbooks-Trans-to-Matlab/books/stats-eight/struct-graph/chapter03/chapter-03-index.graphml'
 G = nx.read_graphml(graphml_file_path)
 
 # 2. 提取节点数据
 node_data = []
 for node_id, data in G.nodes(data=True):
-    # 提取节点ID和所有属性
+    # 初始化row对象
     row = {'id': node_id}
-    row.update(data)
+    
+    # 提取节点ID和需要的属性
+    # 需要提前判断data对象中是否存在「matlab_doc_links」、「label」字段
+    if 'matlab_doc_links' in data:
+        # 判断matlab_doc_links非空
+        # 将matlab_doc_links对象添加到row字段对象中
+        doc_link = data['matlab_doc_links']
+        # 将字符串中的\n内容转化为显示的字符串内容，不将其转意为真是的换行
+        # doc_link = repr(doc_link)
+        replacement = ", "
+        doc_link = doc_link.replace('\n', replacement)
+        row['matlab_doc_links'] = doc_link
+    
+    if 'label' in data:
+        # 判断node_label对象是否非空
+        # 将node_label对象添加到row字段对象中
+        row['label'] = data['label']
+    
     node_data.append(row)
 
 # 3. 提取边数据
